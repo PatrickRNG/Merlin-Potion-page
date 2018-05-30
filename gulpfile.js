2b947457e08aef83e38a0d4eb11b2a464540436a
Patrick Passarella <patrickpassarella@hotmail.com>
2018-05-29 23:33:59 -0300

    Done layout, responsive and half JS

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify-es').default;
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
+const runSequence = require('run-sequence');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+
+const sassSrc = './src/scss/**/*.scss';
+const sassDest = './src/css';
+
+const htmlSrc = './src/*.html';
+
+const imageSrc = 'src/images/**/*.+(png|jpg|gif|svg)';
+const imageDest = 'dist/images';
+
+var prodDest = 'dist';
+
+const sassDev = {
+  outputStyle: 'expanded'
+};
+
+// Comple sass to css and run brower sync
+gulp.task('sass', function () {
+  return gulp.src(sassSrc)
+    .pipe(sass(sassDev).on('error', sass.logError))
+    .pipe(gulp.dest(sassDest))
+    .pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+// Watch the sass files
+gulp.task('watch', function () {
+  return gulp.watch(sassSrc, ['sass']);
+});
+
+// Move the json files to dist
+gulp.task('misc', function () {
+	return gulp.src('./src/*.json')
+		.pipe(gulp.dest('dist/'))
+});
+
+// Move and minify images to dist
+gulp.task('images', function () {
+	return gulp.src(imageSrc)
+		.pipe(cache(imagemin()))
+		.pipe(gulp.dest(imageDest));
+});
+
+// Concatenate all js and css files to one
+gulp.task('useref', function () {
+	gulp.src(htmlSrc)
+		.pipe(useref({searchPath:['./src', '']}))
+		.pipe(gulpIf('*.js', uglify().on('error', function(e){
+			console.log(e);
+		 })))
+		.pipe(gulpIf('*.css', cssnano()))
+		.pipe(gulp.dest('dist/'));
+});
+
+// Browser Sync options and init
+gulp.task('browserSync', () => {
+	browserSync.init({
+		injectChanges: true,
+		server: {
+			baseDir: './src'
+		}
+	});
+});
+
+// Delete dist folder when ran
+gulp.task('clean:dist', function () {
+	return del.sync(prodDest);
+});
+
+gulp.task('default', function (callback) {
+	runSequence(['sass', 'browserSync', 'watch'], callback)
+});
+
+gulp.task('build', function (callback) {
+	runSequence('clean:dist', ['misc', 'useref', 'images'], callback)
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,106 @@
 const { potions } = require('../assets/potions.json');
 
+const potionsEl = document.getElementById('potions__js');
+const modal = document.getElementById('js__modal');
+let data;
+
 function* entries(obj) {
   for (let key of Object.keys(obj)) {
     yield [key, obj[key]];
   }
+  data = obj;
 }
 
 for (let [key, value] of entries(potions)) {
-  console.log(value.name);
+  createPotions(value);
 }
 
-function createItem({name, image, price, effect, ingredients}) {
+potionsEl.childNodes.forEach(el => {
+  el.addEventListener('click', (e) => {
+    let { id } = e.currentTarget;
+    if (!id || !e.currentTarget.id.startsWith('potion_')) return;
+    let modalDataIndex = Number(id.match(/\d$/)[0]);
+	  const modalData = data[modalDataIndex];    
+    modal.style.display = 'block';
+    createModal(modalData);
+  });
+});
 
-  const itemContent = 
-  `
+
+function createPotions({id, name, image, price}) {
+
+  const potionContent = 
+  `<div class="potion" id="potion_${id}">
+    <img class="potion__image" src="assets/products/${image}">
+    <p class="potion__name">${name} -
+    <span>${price}</span>
+    </p>
+  </div>
   `;
 
+  potionsEl.innerHTML += potionContent;
+
 }
+
+function createModal({id, name, image, price, effect, ingredients}) {
+
+  // let ingredientsLi = '';
+  // ingredients.forEach(el => {
+  //   ingredientsLi += `<li>${el}</li>`;
+  // });
+
+  const modalContent =
+  `<div class="modal">
+    <div class="modal__image" style="background-image:url(${image})"></div>
+    <div class="modal__body" id="js_modal_body">
+      <span class="closeBtn" id="js_closeBtn">&times;</span>
+      <h3>${name}</h3>
+      <h3>Use/Effect:</h3>
+      <p>${effect}</p>
+      <h3>Ingredients:</h3>
+      <ul class="modal__ingredients">
+        
+      </ul>
+      <h3>Price:<br>${price}</h3>
+      <div class="btn btn--orange">Add to cart</a>
+    </div>
+  </div>`;
+
+  modal.innerHTML = modalContent;
+
+}
+
+
+
+
 
 function imgError(image) {
   image.onerror = "";
   image.src = "./images/imgError.png";
   return true;
 }
+
+// MOBILE MENU
+
+const menu = document.getElementById('menu__mobile');
+const nav = document.getElementById('nav__js');
+const header = document.querySelector('.header--after');
+const logo = document.querySelector('.header__logo');
+const profile = document.querySelector('.header__profile');
+const input = document.querySelector('.header__input');
+
+menu.addEventListener('click', () => {
+    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+    header.classList.toggle('header--after');
+    menu.classList.toggle('is-active');
+
+    if (nav.style.display === 'flex') {
+      logo.style.display = 'none';
+      profile.style.display = 'none';
+      input.style.display = 'block';
+    } else {
+      logo.style.display = 'block';
+      profile.style.display = 'flex';
+      input.style.display = '';
+    }
+});
